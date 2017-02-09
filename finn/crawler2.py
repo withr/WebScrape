1068e8983e71209d0654e0d09b9331a1ef7a2ebb
Huidong Tian <tienhuitung@gmail.com>
2017-02-09 21:17:56 +0100

    Update crawler2.py

--- finn/crawler2.py
+++ finn/crawler2.py
@@ -6,7 +6,7 @@ from multiprocessing.dummy import Pool
 from bs4 import BeautifulSoup as BS
 
 
-folder = "/home/tian/1T/Finn/pages_8/"
+folder = "/home/tian/1T/Finn/pages_6/"
 shellMSG = subprocess.check_output("find " + folder + " -type f", shell=True)
 html_ls =  shellMSG.split('\n')[:-1]
 N = len(html_ls)
@@ -16,8 +16,8 @@ URL = "https://m.finn.no/lookup.html?finnkode="
 
 def recrawl(i):
     html = html_ls[i]
-    HTML = open(html).read()
-    if len(HTML) < 50:
+    n = subprocess.check_output('ls -l "' + html + '" | awk  \'{print $5}\'', shell=True)
+    if int(n) < 50:
         finn_code = html[-13:-5]
         url = URL + str(finn_code)
         r = requests.get(url, timeout=5)
@@ -29,8 +29,7 @@ def recrawl(i):
 
 for i in range(N):
     recrawl(i)
-    msg = "\rDone: " + str(i/float(N))
+    msg = "\rDone: " + str(i) + "; percent: " + str(i/float(N))
     sys.stdout.write(msg); sys.stdout.flush()
-    
 
-    
+
